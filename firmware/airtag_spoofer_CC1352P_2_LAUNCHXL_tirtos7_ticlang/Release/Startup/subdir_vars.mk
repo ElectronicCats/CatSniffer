f3db501efa3cd2b1ffc1f6553fc0e2fca3dfe8e9
Paulino Calderon <paulino@calderonpale.com>
2023-07-09 14:46:37 -0500

    AirtagSpoofer firmware

---
+++ firmware/airtag_spoofer_CC1352P_2_LAUNCHXL_tirtos7_ticlang/Release/Startup/subdir_vars.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Startup/main.c \
+../Startup/osal_icall_ble.c \
+C:/ti/simplelink_cc13xx_cc26xx_sdk_7_10_01_24/source/ti/ble5stack/rom/agama_r1/rom_init.c 
+
+C_DEPS += \
+./Startup/main.d \
+./Startup/osal_icall_ble.d \
+./Startup/rom_init.d 
+
+OBJS += \
+./Startup/main.o \
+./Startup/osal_icall_ble.o \
+./Startup/rom_init.o 
+
+OBJS__QUOTED += \
+"Startup\main.o" \
+"Startup\osal_icall_ble.o" \
+"Startup\rom_init.o" 
+
+C_DEPS__QUOTED += \
+"Startup\main.d" \
+"Startup\osal_icall_ble.d" \
+"Startup\rom_init.d" 
+
+C_SRCS__QUOTED += \
+"../Startup/main.c" \
+"../Startup/osal_icall_ble.c" \
+"C:/ti/simplelink_cc13xx_cc26xx_sdk_7_10_01_24/source/ti/ble5stack/rom/agama_r1/rom_init.c" 
+
+
