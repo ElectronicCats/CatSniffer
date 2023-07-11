5b9619bcdd39acc6d2543948472847a5aba8f535
Eduardo Contreras <werowero.edy@gmail.com>
2023-07-10 22:33:48 -0500

    add LED animation for upload

--- firmware/SAMD/SerialPassthroughwithboot/SerialPassthroughwithboot.ino
+++ firmware/SAMD/SerialPassthroughwithboot/SerialPassthroughwithboot.ino
@@ -20,24 +20,35 @@
 #define Pin_Reset (1)
 #define Pin_Boot (7)
 #define Pin_Button (23)
-#define LED (0)
+#define LED1 (27)
+#define LED2 (28)
+#define LED3 (0)
 //Mode flag = 1; for bootloader options @ 500000 bauds
 //Mode flag = 0; for passthrough @ 115200 bauds
 bool MODE_FLAG = 0;
+uint8_t LEDs[3]={0,27,28};
+int i=0;
+
+unsigned long interval = 0;    // interval to blink LED
+unsigned long previousMillis = 0;  // will store last time blink happened
 
 void setup() {
   unsigned long baud;
   pinMode(Pin_Button, INPUT_PULLUP);
   pinMode(Pin_Reset, OUTPUT);
   pinMode(Pin_Boot, OUTPUT);
-  pinMode(LED,OUTPUT);
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+  pinMode(LED3,OUTPUT);
 
   //Select mode and speed
   if(!digitalRead(Pin_Button)){
+    interval = 200;
     baud  = 500000;
     MODE_FLAG=1;
   }
   else{
+    interval = 1000; 
     baud = 115200;
     MODE_FLAG=0;
   }
@@ -56,7 +67,9 @@ void setup() {
     delay(100);
     digitalWrite(Pin_Boot, HIGH);
   }
-  digitalWrite(LED, MODE_FLAG);
+  digitalWrite(LED1, 0);
+  digitalWrite(LED2, 0);
+  digitalWrite(LED3, MODE_FLAG);
 }
 
 void loop() {
@@ -68,5 +81,18 @@ void loop() {
   if (Serial1.available()) {     // If anything comes in Serial1 (pins 0 & 1)
     Serial.write(Serial1.read());   // read it and send it out Serial (USB)
   }
+  
+  if(millis() - previousMillis > interval) {
+    previousMillis = millis(); 
+    if(MODE_FLAG){
+      digitalWrite(LEDs[i], !digitalRead(LEDs[i]));
+      i++;
+      if(i>2)i=0;
+    }else{
+      digitalWrite(LED3, !digitalRead(LED3));
+    }
+    
+  }
+
 
 }
